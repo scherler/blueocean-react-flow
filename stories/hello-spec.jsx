ea4097700b97cd6b996186657f973f7118c539f4
Thorsten Scherler <scherler@gmail.com>
2017-12-11 19:20:21 +0100

    [sync_jdl_versions] need to sync react version and related deps

--- stories/hello-spec.jsx
+++ stories/hello-spec.jsx
@@ -4,9 +4,21 @@ import expect from "expect";
 
 const stories = storiesOf('Hello world', module);
 
+function createOptions(text = 'Option', asObject = false) {
+    const options = [];
+
+    for (let index = 0; index < 200; index++) {
+        const label = `${text} ${options.length + 1}`;
+        options.push(!asObject ? label : { label });
+    }
+    return options;
+}
+
 const storyName = 'Simple hello world';
 stories.add(storyName, function() {
-    const story = (<div>Sag hallo</div>);
+    const story = (<div>
+        Sag hallo
+    </div>);
 
     specs(() => describe(storyName, function() {
         let wrapper;
