58c1d2b7a7399ce4ff39f3e704919e496ffdf961
Thorsten Scherler <scherler@gmail.com>
2018-03-16 13:59:32 +0100

    [master] Fix ie11 not supporting Object.assign

--- src/Enum.js
+++ src/Enum.js
@@ -12,20 +12,38 @@
  */
 export class Enum {
 
-  _values = [];
+    _values = [];
 
-  constructor(props) {
-    Object.assign(this, props);
-
-    for (const value in props) {
-      if (props.hasOwnProperty(value)) {
-        this._values.push(props[value]);
-      }
+    constructor(props) {
+        this._addValues(props);
     }
-  }
 
-  values() {
-    return this._values.slice();
-  }
+    extend(extraProps) {
+        const values = this._values.slice();
+        const cloned = new Enum(values);
+        cloned._addValues(extraProps);
+        return cloned;
+    }
+
+    _addValues(props) {
+        // support Arrays or "array-like" objects (e.g. ObservableArray)
+        if (typeof props.slice === 'function' && Array.isArray(props.slice())) {
+            for (const value of props.slice()) {
+                this[value] = value;
+                this._values.push(value);
+            }
+        } else if (typeof props === 'object') {
+            for (const keyName in props) {
+                if (props.hasOwnProperty(keyName)) {
+                    this[keyName] = keyName;
+                    this._values.push(keyName);
+                }
+            }
+        }
+    }
+
+    values() {
+        return this._values.slice();
+    }
 
 }
