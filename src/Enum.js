e1d829be6d8e7c38c3b6bc67507a056fccfc9596
Thorsten Scherler <scherler@gmail.com>
2018-03-17 03:38:23 +0100

    [master] use babel to polyfill object assign and revert alternative implementation

--- src/Enum.js
+++ src/Enum.js
@@ -12,38 +12,20 @@
  */
 export class Enum {
 
-    _values = [];
+  _values = [];
 
-    constructor(props) {
-        this._addValues(props);
-    }
+  constructor(props) {
+    Object.assign(this, props);
 
-    extend(extraProps) {
-        const values = this._values.slice();
-        const cloned = new Enum(values);
-        cloned._addValues(extraProps);
-        return cloned;
+    for (const value in props) {
+      if (props.hasOwnProperty(value)) {
+        this._values.push(props[value]);
+      }
     }
+  }
 
-    _addValues(props) {
-        // support Arrays or "array-like" objects (e.g. ObservableArray)
-        if (typeof props.slice === 'function' && Array.isArray(props.slice())) {
-            for (const value of props.slice()) {
-                this[value] = value;
-                this._values.push(value);
-            }
-        } else if (typeof props === 'object') {
-            for (const keyName in props) {
-                if (props.hasOwnProperty(keyName)) {
-                    this[keyName] = keyName;
-                    this._values.push(keyName);
-                }
-            }
-        }
-    }
-
-    values() {
-        return this._values.slice();
-    }
+  values() {
+    return this._values.slice();
+  }
 
 }
